b44229b71a2370f595bcbf0686db1153871aec48
Lsjljj <14945919+lsjljj@user.noreply.gitee.com>
2025-11-10 21:32:13 +0800

    基于RTX管理账目

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-import sum from '@/test'
 import { RouterProvider } from 'react-router-dom';
+import sum from '@/test'
 import router from './router' 
+import { Provider } from 'react-redux'
+import store from './store'
+
+// 导入定制文件
+import './theme.css'
 
 const Result = sum(1,3)
 console.log(Result);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <RouterProvider router={router} />
+    <Provider store={store}>
+        <RouterProvider router={router} />
+    </Provider>
 );
--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,11 +1,23 @@
+import { Button } from "antd-mobile"
 import { Outlet } from "react-router-dom"
+import { useDispatch } from "react-redux"
+import { useEffect } from "react"
+import { getBillList } from "@/store/modules/billStore"
 
 const Layout = ()=>{
+  const dispatch = useDispatch()
+  useEffect(()=>{
+    dispatch(getBillList())
+  },[dispatch])
   return (
     <div>
       <Outlet/>
       我是首页一级路由
-
+      <Button color="primary">测试全局</Button>
+      <div className="puple">
+        <Button color="primary">测试局部</Button>
+      </div>
+     
     </div>
   )
 }
