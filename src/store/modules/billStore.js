f6d5c6bc974d82a92b41bf87c1f85298a51ea1b3
Lsjljj <14945919+lsjljj@user.noreply.gitee.com>
2025-11-13 23:22:53 +0800

    提交账单

--- src/store/modules/billStore.js
+++ src/store/modules/billStore.js
@@ -12,12 +12,16 @@ const billStore = createSlice({
     // 同步修改方法
     setBillList(state, action) {
       state.billList = action.payload
+    },
+    // 同步添加账单方法
+    addBill(state, action){
+      state.billList.push(action.payload)
     }
   }
 })
 
 // 解构actionCreater函数
-const { setBillList } = billStore.actions
+const { setBillList, addBill } = billStore.actions
 // 编写异步
 const getBillList = ()=>{
   return async (dispatch)=>{
@@ -27,7 +31,17 @@ const getBillList = ()=>{
     dispatch(setBillList(res.data))
   }
 }
-export {getBillList}
+
+const addBillList = (data) => {
+  return async (dispatch)=>{
+    // 编写异步请求
+    const res = await axios.post('http://localhost:8888/ka', data)
+    // 触发同步reducer
+    dispatch(addBill(res.data))
+  }
+}
+
+export {getBillList,addBillList}
 
 // 导入reducer
 const reducer = billStore.reducer
