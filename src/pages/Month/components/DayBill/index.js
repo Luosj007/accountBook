fd04f9c7957f4dba8b1fb17748f1058ed2f22fbf
Lsjljj <14945919+lsjljj@user.noreply.gitee.com>
2025-11-13 22:25:22 +0800

    点击切换账单

--- src/pages/Month/components/DayBill/index.js
+++ src/pages/Month/components/DayBill/index.js
@@ -1,6 +1,6 @@
 import classNames from 'classnames'
 import './index.scss'
-import { useMemo } from 'react'
+import { useMemo, useState } from 'react'
 import { billTypeToName } from '@/contants/index'
 
 const DailyBill = ({date, billList}) => {
@@ -14,12 +14,16 @@ const DailyBill = ({date, billList}) => {
     }
   }, [billList])
 
+  // 控制列表的展示收起
+  const [visible, setVisible] = useState(false)
+
   return (
     <div className={classNames('dailyBill')}>
       <div className="header">
         <div className="dateIcon">
           <span className="date">{date}</span>
-          <span className={classNames('arrow')}></span>
+          {/* expand 这个类名 展开的箭头朝上的样子 */}
+          <span className={classNames('arrow', visible && 'expand')} onClick={()=>setVisible(!visible)}></span>
         </div>
         <div className="oneLineOverview">
           <div className="pay">
@@ -37,7 +41,7 @@ const DailyBill = ({date, billList}) => {
         </div>
       </div>
       {/* 单日列表 */}
-      <div className="billList">
+      <div className="billList" style={{display: visible ? 'block' : 'none'}}>
         {billList.map(item => {
           return (
             <div className="bill" key={item.id}>
